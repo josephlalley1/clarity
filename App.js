a0e538db4ec2d4f3043010910e66aabb82519cb1
josephlalley1 <josephyzyr@gmail.com>
2024-09-21 16:11:30 +0100

    Reformatting and restarting

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import CameraScreen from './App/CameraScreen';
-import VideoLibraryScreen from './App/VideoLibraryScreen';
-
-const Stack = createStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Camera">
-        <Stack.Screen name="Camera" component={CameraScreen} />
-        <Stack.Screen name="Library" component={VideoLibraryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
